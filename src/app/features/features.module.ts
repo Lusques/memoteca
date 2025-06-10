e679316f243df0177ea6744005d423e98bc952e3
Lucas Silva <lucascarlosjobs@gmail.com>
2025-06-10 16:17:42 -0300

    feat(data): creates mock json

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { AddThoughtPageComponent } from './add-thought-page/add-thought-page.component';
 import { ThoughtWallPageComponent } from './thought-wall-page/thought-wall-page.component';
 import { SharedModule } from "../shared/shared.module";
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AddThoughtPageComponent, ThoughtWallPageComponent],
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule, RouterModule, SharedModule],
 })
 export class FeaturesModule {}
