9c848365b6c530e0efeb02a16eaa4e67e085a04c
Lucas Silva <lucascarlosjobs@gmail.com>
2025-06-10 14:10:20 -0300

    feat(styles): add variants

--- src/app/shared/components/thought-card/thought-card.component.ts
+++ src/app/shared/components/thought-card/thought-card.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ThoughtCardSize, ThoughtCardVariant } from '../../types/ui.types';
 
 @Component({
   selector: 'app-thought-card',
   templateUrl: './thought-card.component.html',
-  styleUrls: ['./thought-card.component.scss']
+  styleUrls: ['./thought-card.component.scss'],
 })
 export class ThoughtCardComponent implements OnInit {
-
-  constructor() { }
+  @Input() variant: ThoughtCardVariant = 'dark-blue';
+  @Input() size: ThoughtCardSize = '';
+  imgQuote!: string;
+  constructor() {}
 
   ngOnInit(): void {
+    this.imgQuote = this.getQuoteIconPath();
   }
 
+  getClasses() {
+    const block = 'thought-card';
+    const classes = [`${block}--${this.variant}`];
+    if (this.size) {
+      classes.push(`${block}--${this.size}`);
+    }
+    return classes;
+  }
+  getQuoteIconPath() {
+    return `/assets/icons/icon-quote-${this.variant}.svg`;
+  }
 }
---
+++ src/app/shared/types/ui.types.ts
@@ -0,0 +1,2 @@
+export type ThoughtCardVariant = 'dark-blue' | 'sky-blue' | 'mint-green';
+export type ThoughtCardSize = 'large' | '';
